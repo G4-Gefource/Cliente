497f0c02726996faaa2681a4486e635ccbd61fe5
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2022-10-31 14:08:01 -0300

    Update prototipo_app_1.py

--- prototipo_app_1.py
+++ prototipo_app_1.py
@@ -12,24 +12,21 @@ import bot_es
 auxiliar = True
 while auxiliar is True:
     funcoes.limpa()
-    print('Informe o voucher recebido em seu checkin:')
-    print('Informar el voucher recibido en el check-in:')
-    voucher = input('')
+    print('Bem vindo ao Eventum!')
+    print('Bienvenido al Eventum!')
+    print('\nGostaria da versão em espanhol?')
+    print('¿Te Gustaria la version en español?')
+    # UPPER() -> TRANSFORMA TODAS OS CARACTERES INSERIDOS EM MAIÚSCULO
+    idioma = input('S - Sim(Sí)\nN - Não(No)\n').upper()
     funcoes.limpa()
-    if voucher == '000':
+    if idioma[0] == 'N':
         op = 1  # VARIÁVEL AUXILIAR PARA LAÇO DE REPETIÇÃO
         while op != 0:
+            print('Você selecionou a opção em Português!\n')
+            print('Informe o voucher recebido em seu checkin:')
+            voucher = input('')
             funcoes.limpa()
-            print('Bem vindo ao Eventum!')
-            print('Bienvenido al Eventum!')
-            print('\nGostaria da versão em espanhol?')
-            print('¿Te Gustaria la version en español?')
-            # UPPER() -> TRANSFORMA TODAS OS CARACTERES INSERIDOS EM MAIÚSCULO
-            idioma = input('S - Sim(Sí)\nN - Não(No)\n').upper()
-            funcoes.limpa()
-
-            if idioma[0] == 'N':  # COMPARA APENAS A PRIMEIRA LETRA ESCRITA PELO USUÁRIO
-                print('Você selecionou a opção em Português!\n')
+            if voucher == '000':  # COMPARA APENAS A PRIMEIRA LETRA ESCRITA PELO USUÁRIO
                 opt = 1
                 while opt != 0:
                     funcoes.menu_pt()   # CHAMA A FUNÇÃO MENU_PT
@@ -77,66 +74,70 @@ while auxiliar is True:
                         print('Opção inválida.')
                         funcoes.pausa_pt()
                         funcoes.limpa()
-            elif idioma[0] == 'S':  # COMPARA APENAS A PRIMEIRA LETRA ESCRITA PELO USUÁRIO
-                print('¡Ha seleccionado la opción en Español!')
-                opt = 1
-                while opt != 0:
-                    funcoes.menu_es()   # CHAMA A FUNÇÃO MENU_ES
-                    opt = int(input('Seleccione una de las opciones y presione ENTER: '))
-                    funcoes.limpa()
-                    if opt == 1:
-                        aux = 1
-                        while aux != 0:
-                            pt = input(
-                                '¿Ya sabes el tipo de lugar turístico que estás buscando?\n(S - Sí/N - No)\n')
+            else:
+                print('Este voucher não é válido, por favor tente novamente.')
+                print('Se o problema persistir entre em contato com um funcionário do hotel.')
+                funcoes.pausa_pt()
+    elif idioma[0] == 'S':
+        print('¡Ha seleccionado la opción en Español!\n')
+        opt = 1
+        while opt != 0:
+            print('Informar el voucher recibido en el check-in:')
+            voucher = input('')
+            funcoes.limpa()
+            if voucher == '000':
+                funcoes.menu_es()   # CHAMA A FUNÇÃO MENU_ES
+                opt = int(input('Seleccione una de las opciones y presione ENTER: '))
+                funcoes.limpa()
+                if opt == 1:
+                    aux = 1
+                    while aux != 0:
+                        pt = input(
+                            '¿Ya sabes el tipo de lugar turístico que estás buscando?\n(S - Sí/N - No)\n')
+                        funcoes.limpa()
+                        if (pt[0]).upper() == 'S':
+                            print('Joya.')
+                            aux = int(input('Pulsa 0 para salir: '))
                             funcoes.limpa()
-                            if (pt[0]).upper() == 'S':
-                                print('Joya.')
-                                aux = int(input('Pulsa 0 para salir: '))
-                                funcoes.limpa()
-                            elif (pt[0]).upper() == 'N':
-                                print(
-                                    '¿Le gustaría la ayuda de BOT para asegurarle una experiencia personalizada?')
-                                bot = input('(S - Sí/N - No)\n')
-                                if (bot[0]).upper() == 'S':
-                                    bot_es.chat()
-                                elif (bot[0]).upper() == 'N':
-                                    print('')
-                                else:
-                                    print('Opción no válida.')
-                                    funcoes.pausa_es()
-                                    funcoes.limpa()
+                        elif (pt[0]).upper() == 'N':
+                            print(
+                                '¿Le gustaría la ayuda de BOT para asegurarle una experiencia personalizada?')
+                            bot = input('(S - Sí/N - No)\n')
+                            if (bot[0]).upper() == 'S':
+                                bot_es.chat()
+                            elif (bot[0]).upper() == 'N':
+                                print('')
                             else:
                                 print('Opción no válida.')
-                                aux = int(input('Pulsa 0 para salir: '))
+                                funcoes.pausa_es()
                                 funcoes.limpa()
-                    elif opt == 2:
-                        print(
-                            'Informe el lugar turístico registrado que desea registrar.')
-                    elif opt == 3:
-                        print('Informe al hotel donde se hospedó.')
-                    elif opt == 4:
-                        print('Deja tus comentarios de Eventum aquí.')
-                    elif opt == 0:
-                        print('¡Gracias por usar Eventum!')
-                        funcoes.pausa_es()
-                        op = 0
-                        auxiliar = False
-                    else:
-                        print('Opción no válida.')
-                        funcoes.pausa_es()
-                        funcoes.limpa()
+                        else:
+                            print('Opción no válida.')
+                            aux = int(input('Pulsa 0 para salir: '))
+                            funcoes.limpa()
+                elif opt == 2:
+                    print(
+                        'Informe el lugar turístico registrado que desea registrar.')
+                elif opt == 3:
+                    print('Informe al hotel donde se hospedó.')
+                elif opt == 4:
+                    print('Deja tus comentarios de Eventum aquí.')
+                elif opt == 0:
+                    print('¡Gracias por usar Eventum!')
+                    funcoes.pausa_es()
+                    op = 0
+                    auxiliar = False
+                else:
+                    print('Opción no válida.')
+                    funcoes.pausa_es()
+                    funcoes.limpa()
             else:
-                print('Opção inválida inserida.')
-                print('Opción no válida ingresada.')
-                print('Pressione qualquer tecla para continuar...\n')
-                print('Pulse cualquier tecla para continuar...\n')
-                char = msvcrt.getch()
+                print('Este cupón no es válido, inténtalo de nuevo.')
+                print('Si el problema persiste, comuníquese con un empleado del hotel.')
+                funcoes.pausa_es()
     else:
-        print('Este voucher não é válido, por favor tente novamente.')
-        print('Se o problema persistir entre em contato com um funcionário do hotel.')
-        print('\nEste voucher no es válido, inténtalo de nuevo.')
-        print('Si el problema persiste, comuníquese con un empleado del hotel.')
-        print('\nPressione qualquer tecla para continuar...\n')
+        print('Opção inválida inserida.')
+        print('Opción no válida ingresada.')
+        print('Pressione qualquer tecla para continuar...\n')
         print('Pulse cualquier tecla para continuar...\n')
         char = msvcrt.getch()
