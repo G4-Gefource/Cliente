bce2995fa714c472dd83b4a002ff2979e087c669
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2022-11-02 20:44:42 -0300

    atualizações gerais
    exclusão da segunda funcionalidade, criação da funcionalidade de avaliação do hotel.

--- prototipo_totem_1.py
+++ prototipo_totem_1.py
@@ -55,10 +55,59 @@ while op != 0:
                         aux = int(input('Pressione 0 para sair: '))
                         funcoes.limpa()
             elif opt == 2:
-                print('Informe o ponto turístico registrado que deseja dar feedback.')
+                hotel = 1
+                while hotel != 0:
+                    v = ['Como você avalia sua estadia no hotel?', 'Como você avalia seu conforto no hotel?',
+                         'Como você avalia a localização do hotel?', 'Como você avalia a limpeza do hotel?',
+                         'Como você avalia o atendimento dos funcionários?']
+                    print('1 - Saint Patrick Praia Hotel')
+                    print('2 - Hotel Des Basques')
+                    print('3 - Pousada Nossa Casa')
+                    print('4 - Saint Patrick Grand Hotel')
+                    hotel = int(
+                        input(('Informe o hotel em que ficou hospedado: ')))
+                    hosped = ''
+                    if hotel == 1:
+                        hosped = 'Saint Patrick Praia Hotel'
+                    elif hotel == 2:
+                        hosped = 'Hotel Des Basques'
+                    elif hotel == 3:
+                        hosped = 'Pousada Nossa Casa'
+                    elif hotel == 4:
+                        hosped = 'Saint Patrick Grand Hotel'
+                    elif hotel == 0:
+                        print('Operação encerrada pelo usuário.\n')
+                        funcoes.pausa_pt()
+                        funcoes.limpa()
+                    else:
+                        print('Opção inválida.')
+                        funcoes.pausa_pt()
+                        funcoes.limpa()
+                    print(f'Responda as perguntas sobre o hotel {hosped}.\n')
+                    soma = 0.0
+                    i = 0
+                    while i in range(len(v)):
+                        print(v[i])
+                        a = float(input('Informe uma nota de 0 a 5: '))
+                        if a > 5 or a < 0:
+                            print('Esta nota não pode ser inserida.')
+                            i = i - 1
+                        else:
+                            soma = soma + a
+                            i = i + 1
+                    media = soma / len(v)
+                    print(
+                        f'Você avaliou o hotel {hosped} com {media:.2f} estrelas.')
+                    print(
+                        f'Gostaria de deixar um comentário sobre o hotel {hosped}?')
+                    av = input('').upper()
+                    if av[0] == 'S':
+                        coment = input('Informe aqui seu comentário: ')
+                    print('Obrigado pela sua avaliação!')
+                    hotel = 0
+                    funcoes.pausa_pt()
+                    funcoes.limpa()
             elif opt == 3:
-                print('Informe o hotel em que ficou hospedado.')
-            elif opt == 4:
                 print('Deixe aqui seu Feedback do Eventum.')
             elif opt == 0:
                 print('Obrigado por usar o Eventum!')
@@ -78,14 +127,16 @@ while op != 0:
             if opt == 1:
                 aux = 1
                 while aux != 0:
-                    pt = input('¿Ya sabes el tipo de lugar turístico que estás buscando?\n(S - Sí/N - No)\n')
+                    pt = input(
+                        '¿Ya sabes el tipo de lugar turístico que estás buscando?\n(S - Sí/N - No)\n')
                     funcoes.limpa()
                     if (pt[0]).upper() == 'S':
                         print('Joya.')
                         aux = int(input('Pulsa 0 para salir: '))
                         funcoes.limpa()
                     elif (pt[0]).upper() == 'N':
-                        print('¿Le gustaría la ayuda de BOT para asegurarle una experiencia personalizada?')
+                        print(
+                            '¿Le gustaría la ayuda de BOT para asegurarle una experiencia personalizada?')
                         bot = input('(S - Sí/N - No)\n')
                         if (bot[0]).upper() == 'S':
                             bot_es.chat()
@@ -100,10 +151,60 @@ while op != 0:
                         aux = int(input('Pulsa 0 para salir: '))
                         funcoes.limpa()
             elif opt == 2:
-                print('Informe el lugar turístico registrado que desea registrar.')
+                hotel = 1
+                while hotel != 0:
+                    v = ['¿Cómo califica su estancia en el hotel?', '¿Cómo califica su comodidad en el hotel?',
+                         '¿Cómo califica la ubicación del hotel?', '¿Cómo califica la limpieza del hotel?',
+                         '¿Cómo califica el servicio de los empleados?']
+                    print('1 - Saint Patrick Praia Hotel')
+                    print('2 - Hotel Des Basques')
+                    print('3 - Pousada Nossa Casa')
+                    print('4 - Saint Patrick Grand Hotel')
+                    hotel = int(
+                        input('Informar al hotel donde se hospedó: '))
+                    hosped = ''
+                    if hotel == 1:
+                        hosped = 'Saint Patrick Praia Hotel'
+                    elif hotel == 2:
+                        hosped = 'Hotel Des Basques'
+                    elif hotel == 3:
+                        hosped = 'Pousada Nossa Casa'
+                    elif hotel == 4:
+                        hosped = 'Saint Patrick Grand Hotel'
+                    elif hotel == 0:
+                        print('Operación terminada por el usuario.\n')
+                        funcoes.pausa_es()
+                        funcoes.limpa()
+                    else:
+                        print('Opción no válida.')
+                        funcoes.pausa_es()
+                        funcoes.limpa()
+                    print(f'Responder preguntas sobre el hotel {hosped}.\n')
+                    soma = 0.0
+                    i = 0
+                    while i in range(len(v)):
+                        print(v[i])
+                        a = float(
+                            input('Introduzca una puntuación de 0 a 5: '))
+                        if a > 5 or a < 0:
+                            print('Esta nota no se puede ingresar.')
+                            i = i - 1
+                        else:
+                            soma = soma + a
+                            i = i + 1
+                    media = soma / len(v)
+                    print(
+                        f'Ha calificado el hotel {hosped} con {media:.2f} estrellas.')
+                    print(
+                        f'¿Le gustaría dejar una reseña sobre el hotel {hosped}?')
+                    av = input('').upper()
+                    if av[0] == 'S':
+                        coment = input('Ingrese su comentario aquí: ')
+                    print('¡Gracias por tu valoración!')
+                    hotel = 0
+                    funcoes.pausa_es()
+                    funcoes.limpa()
             elif opt == 3:
-                print('Informe al hotel donde se hospedó.')
-            elif opt == 4:
                 print('Deja tus comentarios de Eventum aquí.')
             elif opt == 0:
                 print('¡Gracias por usar Eventum!')
