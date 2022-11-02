c20b43bbcd471c02efd44e04a40dbab1b4be2426
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2022-11-02 20:41:37 -0300

    alteração nos menus
    mudança nos menus, exclusão da funcionalidade 2, linhas 23 e 30

--- funcoes.py
+++ funcoes.py
@@ -20,15 +20,13 @@ def pausa_es():  # FUNÇÃO PARA PAUSAR O SISTEMA EM ESPANHOL
 
 def menu_pt():  # FUNÇÃO PARA MENU EM PORTUGUÊS
     print('1 - Encontrar pontos turísticos\n')
-    print('2 - Dar feedback sobre visita\n')
-    print('3 - Dar feedback sobre o hotel\n')
-    print('4 - Dar feedback sobre o Eventum\n')
+    print('2 - Dar feedback sobre o hotel\n')
+    print('3 - Dar feedback sobre o Eventum\n')
     print('0 - Sair\n')
 
 
 def menu_es():  # FUNÇÃO PARA MENU EM ESPANHOL
     print('1 - Encontrar lugares de interés\n')
-    print('2 - Dar su opinión sobre la visita\n')
-    print('3 - Dar su opinión sobre el hotel\n')
-    print('4 - Dar su opinión sobre el Eventum\n')
+    print('2 - Dar su opinión sobre el hotel\n')
+    print('3 - Dar su opinión sobre el Eventum\n')
     print('0 - Salir\n')
