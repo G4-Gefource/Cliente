cbbf0b4f20ce52895e823ae88086b4672c47ec71
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2022-11-07 21:07:40 -0300

    criação de funções
    criada a função para exibir pontos turísticos em português (35 - 41) e em espanhol (44 - 50)

--- funcoes.py
+++ funcoes.py
@@ -30,3 +30,21 @@ def menu_es():  # FUNÇÃO PARA MENU EM ESPANHOL
     print('2 - Dar su opinión sobre el hotel\n')
     print('3 - Dar su opinión sobre el Eventum\n')
     print('0 - Salir\n')
+
+    
+    def exibe_ponto_pt(ponto):  # FUNÇÃO PARA EXIBIR PONTO TURÍSTICO EM PORTUGUÊS
+    print(
+        f'Acho que o passeio perfeito para vocês seria o {(ponto["NOME"]).title()}!')
+    print(
+        f'O {(ponto["NOME"]).title()} tem uma nota de {ponto["NOTA"]} nos sites de avaliação!')
+    print(
+        f'Além disso, você o encontra a {ponto["LOCALIZAÇÃO"]} do hotel!')
+
+
+def exibe_ponto_es(ponto):  # FUNÇÃO PARA EXIBIR PONTO TURÍSTICO EM ESPANHOL
+    print(
+        f'¡Creo que el tour perfecto para ti sería el {(ponto["NOME"]).title()}!')
+    print(
+        f'¡El {(ponto["NOME"]).title()} tiene una nota de {ponto["NOTA"]} en los sitios de revisión!')
+    print(
+        f'¡Además, puedes encontrar {ponto["LOCALIZAÇÃO"]} del hotel!')
