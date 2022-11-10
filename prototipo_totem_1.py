f299e12e4b3fd0a9dfb751dbcda2ac66d64c62fe
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2022-11-10 19:21:58 -0300

    atualização geral
    importação do novo código

--- prototipo_totem_1.py
+++ prototipo_totem_1.py
@@ -1,6 +1,5 @@
 # PRIMEIRO PROTÓTIPO DA VERSÃO DE TOTEM DO CLIENTE DO EVENTUM
 
-import os
 import msvcrt
 import funcoes  # CHAMA AS FUNÇÕES PARA O PROJETO
 import bot_pt
@@ -8,9 +7,43 @@ import bot_es
 
 # INÍCIO DO CÓDIGO
 
-
+pontos = [{'NOME': 'RESTAURANTE A',
+           'NOTA': 9.45, 'LOCALIZAÇÃO': '300m de distância',
+           'DESCRIÇÃO_PT':
+           'Restaurante familiar, bem localizado, com uma grande variedade de comidas regionais.',
+           'DESCRIÇÃO_ES':
+           'Restaurante familiar, bien ubicado, con una amplia variedad de comidas regionales.'},
+          {'NOME': 'BAR B', 'NOTA': 9.05,
+           'LOCALIZAÇÃO': '600m de distância',
+           'DESCRIÇÃO_PT':
+           'Bar conhecido por noites com música e preço baixo.',
+           'DESCRIÇÃO_ES':
+           'Bar conocido por las noches con música y precios bajos.'},
+          {'NOME': 'PRAIA C', 'NOTA': 8.75,
+           'LOCALIZAÇÃO': '100m de distância',
+           'DESCRIÇÃO_PT':
+           'Uma das praias mais procuradas de Maceió, o local perfeito para aproveitar um dia de sol.',
+           'DESCRIÇÃO_ES':
+           'Una de las playas más solicitadas de Maceió, el lugar perfecto para disfrutar de un día soleado.'},
+          {'NOME': 'PASSEIO D', 'NOTA': 8.50,
+           'LOCALIZAÇÃO': '1km de distância',
+           'DESCRIÇÃO_PT':
+           'Um passeio inesquecível para Maceió, preço baixo e melhor qualidade.',
+           'DESCRIÇÃO_ES':
+           'Un tour inolvidable a Maceió, bajo precio y la mejor calidad.'},
+          {'NOME': 'CAFÉ E', 'NOTA': 9.85,
+           'LOCALIZAÇÃO': '400m de distância',
+           'DESCRIÇÃO_PT':
+           'Ambiente familiar, com uma grande variedade de refeições para agradar todos os gostos.',
+           'DESCRIÇÃO_ES':
+           'Ambiente familiar, con una gran variedad de comidas para todos los gustos.'},
+          {'NOME': 'LUAU F', 'NOTA': 7.95,
+           'LOCALIZAÇÃO': '800m de distância',
+           'DESCRIÇÃO_PT':
+           'Um ambiente ideal para casais que querem aproveitar o Maceió da forma mais romântica.',
+           'DESCRIÇÃO_ES':
+           'Un entorno ideal para parejas que quieran disfrutar de Maceió de la forma más romántica.'}]
 op = 1  # VARIÁVEL AUXILIAR PARA LAÇO DE REPETIÇÃO
-
 while op != 0:
     funcoes.limpa()
     print('Bem vindo ao Eventum!')
@@ -35,31 +68,138 @@ while op != 0:
                         'Você já sabe o tipo de ponto turístico que está procurando?\n(S - Sim/N - Não)\n')
                     funcoes.limpa()
                     if (pt[0]).upper() == 'S':
-                        print('Jóia.')
-                        aux = int(input('Pressione 0 para sair: '))
+                        aux_ponto = True
+                        while aux_ponto:
+                            procura = input(
+                                'Insira o nome ponto que está procurando: ').upper()
+                            ponto_local = list(
+                                filter(lambda p: p['NOME'] == procura, pontos))
+                            funcoes.limpa()
+                            if len(ponto_local) < 1:
+                                print('Este ponto não está registrado.')
+                                funcoes.pausa_pt()
+                                funcoes.limpa()
+                            else:
+                                funcoes.exibir_pt(ponto_local[0])
+                                print('Gostaria de um arquivo sobre esse ponto?')
+                                arq = input('(S - Sim/N - Não)\n').upper()
+                                if arq[0] == 'S':
+                                    print('Gerando arquivo...')
+                                    funcoes.pausa_pt()
+                                    aux_ponto = False
+                                else:
+                                    print('Gostaria de procurar outro ponto?')
+                                    novo = input('(S - Sim/N - Não)\n').upper()
+                                    if novo[0] == 'S':
+                                        funcoes.limpa()
+                                    else:
+                                        print('Operação encerrada.')
+                                        funcoes.pausa_pt()
+                                        aux_ponto = False
                         funcoes.limpa()
                     elif (pt[0]).upper() == 'N':
                         print(
-                            'Gostaria da ajuda do BOT para garantir uma experiência personalizada para você?')
-                        bot = input('(S - Sim/N - Não)\n')
+                            'Gostaria da ajuda da Iara para garantir uma experiência personalizada para você?')
+                        bot = input('')
+                        funcoes.limpa()
                         if (bot[0]).upper() == 'S':
                             bot_pt.chat()
-                        elif (bot[0]).upper() == 'N':
-                            print('')
-                        else:
-                            print('Opção inválida.')
-                            funcoes.pausa_pt()
-                            funcoes.limpa()
                     else:
                         print('Opção inválida.')
-                        aux = int(input('Pressione 0 para sair: '))
+                        funcoes.pausa_pt()
                         funcoes.limpa()
+                    aux = 0
             elif opt == 2:
-                print('Informe o ponto turístico registrado que deseja dar feedback.')
+                hotel = 1
+                while hotel != 0:
+                    v = ['Como você avalia sua estadia no hotel?', 'Como você avalia seu conforto no hotel?',
+                         'Como você avalia a localização do hotel?', 'Como você avalia a limpeza do hotel?',
+                         'Como você avalia o atendimento dos funcionários?']
+                    print('1 - Saint Patrick Praia Hotel')
+                    print('2 - Hotel Des Basques')
+                    print('3 - Pousada Nossa Casa')
+                    print('4 - Saint Patrick Grand Hotel')
+                    hotel = int(
+                        input(('Informe o hotel em que ficou hospedado: ')))
+                    hosped = ''
+                    if hotel == 1:
+                        hosped = 'Saint Patrick Praia Hotel'
+                    elif hotel == 2:
+                        hosped = 'Hotel Des Basques'
+                    elif hotel == 3:
+                        hosped = 'Pousada Nossa Casa'
+                    elif hotel == 4:
+                        hosped = 'Saint Patrick Grand Hotel'
+                    elif hotel == 0:
+                        print('Operação encerrada pelo usuário.\n')
+                        funcoes.pausa_pt()
+                        funcoes.limpa()
+                    else:
+                        print('Opção inválida.')
+                        funcoes.pausa_pt()
+                        funcoes.limpa()
+                    print(f'Responda as perguntas sobre o hotel {hosped}.\n')
+                    soma = 0.0
+                    i = 0
+                    while i in range(len(v)):
+                        print(v[i])
+                        a = float(input('Informe uma nota de 0 a 5: '))
+                        if a > 5 or a < 0:
+                            print('Esta nota não pode ser inserida.')
+                            i = i - 1
+                        else:
+                            soma = soma + a
+                            i = i + 1
+                    media = soma / len(v)
+                    file = open('notas_hotel.txt', 'a')
+                    file.write(str(media)+"\n")
+                    file.close()
+                    print(
+                        f'Você avaliou o hotel {hosped} com {media:.2f} estrelas.')
+                    print(
+                        f'Gostaria de deixar um comentário sobre o hotel {hosped}?')
+                    av = input('').upper()
+                    if av[0] == 'S':
+                        coment = input('Informe aqui seu comentário: ')
+                        arquivo = open('comentario_hotel.txt',
+                                       'a', encoding='utf-8')
+                        arquivo.write(coment+"\n")
+                        arquivo.close()
+                    print('Obrigado pela sua avaliação!')
+                    hotel = 0
+                    funcoes.pausa_pt()
+                    funcoes.limpa()
             elif opt == 3:
-                print('Informe o hotel em que ficou hospedado.')
-            elif opt == 4:
-                print('Deixe aqui seu Feedback do Eventum.')
+                v = ['Como você avalia a facilidade de navegação do Eventum?',
+                     'Como você avalia o design do Eventum?', 'Como você avalia a praticidade do Eventum?',
+                     'Quão satisfeito você está com o uso do Eventum?']
+                i = 0
+                soma = 0.0
+                while i in range(len(v)):
+                    print(v[i])
+                    a = float(input('Informe uma nota de 0 a 10: '))
+                    if a > 10 or a < 0:
+                        print('Esta nota não pode ser inserida.')
+                        i = i - 1
+                    else:
+                        soma = soma + a
+                        i = i + 1
+                media = soma / len(v)
+                file = open('notas_eventum.txt', 'a')
+                file.write(str(media)+"\n")
+                file.close()
+                print(f'Você avaliou o Eventum com média {media:.2f}.')
+                print(f'Gostaria de deixar um comentário sobre o Eventum?')
+                av = input('').upper()
+                if av[0] == 'S':
+                    coment = input('Informe aqui seu comentário: ')
+                    arquivo = open('comentario_eventum.txt',
+                                   'a', encoding='utf-8')
+                    arquivo.write(coment+"\n")
+                    arquivo.close()
+                print('Obrigado pela sua avaliação!')
+                funcoes.pausa_pt()
+                funcoes.limpa()
             elif opt == 0:
                 print('Obrigado por usar o Eventum!')
                 funcoes.pausa_pt()
@@ -78,33 +218,148 @@ while op != 0:
             if opt == 1:
                 aux = 1
                 while aux != 0:
-                    pt = input('¿Ya sabes el tipo de lugar turístico que estás buscando?\n(S - Sí/N - No)\n')
-                    funcoes.limpa()
-                    if (pt[0]).upper() == 'S':
-                        print('Joya.')
-                        aux = int(input('Pulsa 0 para salir: '))
+                    aux = 1
+                    while aux != 0:
+                        pt = input(
+                            '¿Ya sabes el tipo de lugar turístico que estás buscando?\n(S - Si/N - No)\n')
                         funcoes.limpa()
-                    elif (pt[0]).upper() == 'N':
-                        print('¿Le gustaría la ayuda de BOT para asegurarle una experiencia personalizada?')
-                        bot = input('(S - Sí/N - No)\n')
-                        if (bot[0]).upper() == 'S':
-                            bot_es.chat()
-                        elif (bot[0]).upper() == 'N':
-                            print('')
+                        if (pt[0]).upper() == 'S':
+                            aux_ponto = True
+                            while aux_ponto:
+                                procura = input(
+                                    'Introduzca el nombre del punto que está buscando: ').upper()
+                                ponto_local = list(
+                                    filter(lambda p: p['NOME'] == procura, pontos))
+                                funcoes.limpa()
+                                if len(ponto_local) < 1:
+                                    print('Este punto no está registrado.')
+                                    funcoes.pausa_es()
+                                    funcoes.limpa()
+                                else:
+                                    funcoes.exibir_es(ponto_local[0])
+                                    print(
+                                        '¿Desea un archivo sobre este punto?')
+                                    arq = input('(S - Si/N - No)\n').upper()
+                                    if arq[0] == 'S':
+                                        print('Generando archivo...')
+                                        funcoes.pausa_es()
+                                        aux_ponto = False
+                                    else:
+                                        print(
+                                            '¿Te gustaría buscar otro punto?')
+                                        novo = input(
+                                            '(S - Si/N - No)\n').upper()
+                                        if novo[0] == 'S':
+                                            funcoes.limpa()
+                                        else:
+                                            print('Operación cerrada.')
+                                            funcoes.pausa_es()
+                                            aux_ponto = False
+                            funcoes.limpa()
+                        elif (pt[0]).upper() == 'N':
+                            print(
+                                'Gostaria da ajuda da Iara para garantir uma experiência personalizada para você?')
+                            bot = input('')
+                            funcoes.limpa()
+                            if (bot[0]).upper() == 'S':
+                                bot_es.chat()
                         else:
-                            print('Opción no válida.')
+                            print('Opção inválida.')
                             funcoes.pausa_es()
                             funcoes.limpa()
+                        aux = 0
+            elif opt == 2:
+                hotel = 1
+                while hotel != 0:
+                    v = ['¿Cómo califica su estancia en el hotel?', '¿Cómo califica su comodidad en el hotel?',
+                         '¿Cómo califica la ubicación del hotel?', '¿Cómo califica la limpieza del hotel?',
+                         '¿Cómo califica el servicio de los empleados?']
+                    print('1 - Saint Patrick Praia Hotel')
+                    print('2 - Hotel Des Basques')
+                    print('3 - Pousada Nossa Casa')
+                    print('4 - Saint Patrick Grand Hotel')
+                    hotel = int(
+                        input('Informar al hotel donde se hospedó: '))
+                    hosped = ''
+                    if hotel == 1:
+                        hosped = 'Saint Patrick Praia Hotel'
+                    elif hotel == 2:
+                        hosped = 'Hotel Des Basques'
+                    elif hotel == 3:
+                        hosped = 'Pousada Nossa Casa'
+                    elif hotel == 4:
+                        hosped = 'Saint Patrick Grand Hotel'
+                    elif hotel == 0:
+                        print('Operación terminada por el usuario.\n')
+                        funcoes.pausa_es()
+                        funcoes.limpa()
                     else:
                         print('Opción no válida.')
-                        aux = int(input('Pulsa 0 para salir: '))
+                        funcoes.pausa_es()
                         funcoes.limpa()
-            elif opt == 2:
-                print('Informe el lugar turístico registrado que desea registrar.')
+                    print(f'Responder preguntas sobre el hotel {hosped}.\n')
+                    soma = 0.0
+                    i = 0
+                    while i in range(len(v)):
+                        print(v[i])
+                        a = float(
+                            input('Introduzca una puntuación de 0 a 5: '))
+                        if a > 5 or a < 0:
+                            print('Esta nota no se puede ingresar.')
+                            i = i - 1
+                        else:
+                            soma = soma + a
+                            i = i + 1
+                    media = soma / len(v)
+                    file = open('notas_hotel.txt', 'a')
+                    file.write(str(media)+"\n")
+                    file.close()
+                    print(
+                        f'Ha calificado el hotel {hosped} con {media:.2f} estrellas.')
+                    print(
+                        f'¿Le gustaría dejar una reseña sobre el hotel {hosped}?')
+                    av = input('').upper()
+                    if av[0] == 'S':
+                        coment = input('Ingrese su comentario aquí: ')
+                        arquivo = open('comentario_hotel.txt',
+                                       'a', encoding='utf-8')
+                        arquivo.write(coment+"\n")
+                        arquivo.close()
+                    print('¡Gracias por tu valoración!')
+                    hotel = 0
+                    funcoes.pausa_es()
+                    funcoes.limpa()
             elif opt == 3:
-                print('Informe al hotel donde se hospedó.')
-            elif opt == 4:
-                print('Deja tus comentarios de Eventum aquí.')
+                v = ['¿Cómo califica la facilidad de navegación de Eventum?',
+                     '¿Cómo valoras el diseño de Eventum?', '¿Cómo califica la practicidad de Eventum?',
+                     '¿Qué tan satisfecho está con el uso de Eventum?']
+                i = 0
+                soma = 0.0
+                while i in range(len(v)):
+                    print(v[i])
+                    a = float(input('Introduzca una puntuación de 0 a 10: '))
+                    if a > 10 or a < 0:
+                        print('Esta nota no se puede ingresar.')
+                        i = i - 1
+                    else:
+                        soma = soma + a
+                        i = i + 1
+                media = soma / len(v)
+                file = open('notas_eventum.txt', 'a')
+                file.write(str(media)+"\n")
+                file.close()
+                print(f'Calificaste Eventum con promedio {media:.2f}.')
+                print(f'¿Quieres dejar un comentario sobre Eventum?')
+                av = input('').upper()
+                if av[0] == 'S':
+                    coment = input('Ingrese su comentario aquí: ')
+                    arquivo = open('comentario_eventum.txt',
+                                   'a', encoding='utf-8')
+                    arquivo.write(coment+"\n")
+                    arquivo.close()
+                print('¡Gracias por tu valoración!')
+                funcoes.pausa_es()
+                funcoes.limpa()
             elif opt == 0:
                 print('¡Gracias por usar Eventum!')
                 funcoes.pausa_es()
